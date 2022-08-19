fdbc0fbfac660d18819170e2c24f1a08f28de91e
Jean <acalto2@gmail.com>
2022-08-19 23:59:15 +0200

    init ok

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,23 @@
+//import newArr from '../index';
+//import newArr from '..';
+
+
+
+
+//const numArr = [3, 4, 5, 6];
+//const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+/*
+xit('should make a new array containing dog', () => {
+    expect(newArr(3, wordArr)).toContain('dog');
+});
+xit('make a new array containing 3', () => {
+    expect(newArr(3, wordArr)).toContain(3);
+});
+
+*/
+
+
+
+
+
+
